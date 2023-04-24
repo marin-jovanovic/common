7754ed148e02bf4bfff1d3b962d753a28285d725
marin-jovanovic <marin.jovanovic12@gmail.com>
2023-04-24 16:01:25 +0200

    cleanup

--- git.sh
+++
@@ -1,8 +0,0 @@
-git add /home/kim/Documents/github/marin-jovanovic/web-dev/pacman/scripts/animations.js
-git commit -m "pacman scripts animations.js - wip"
-git add /home/kim/Documents/github/marin-jovanovic/web-dev/pacman/scripts/keys.js
-git commit -m "pacman scripts keys.js - wip"
-git add /home/kim/Documents/github/marin-jovanovic/web-dev/pacman/scripts/canvas_level_adapter.js
-git commit -m "pacman scripts canvas_level_adapter.js - wip"
-git add /home/kim/Documents/github/marin-jovanovic/web-dev/pacman/scripts/index.js
-git commit -m "pacman scripts index.js - wip"
