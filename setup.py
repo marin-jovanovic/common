0c99a831015be3b00ed22c0a5955b4a42192e5ec
marin-jovanovic <marin.jovanovic12@gmail.com>
2024-10-31 01:35:16 +0100

    rename package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='comm',
+    name='commmj',
     packages=find_packages(include=['comm']),
     version='0.1.0',
     description='Common code snippets',
